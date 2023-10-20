2a465f25fdfe7fcff397dbbd324936c112d3829c
JoyceChen1106 <joyce1106chen2@gmail.com>
2023-10-20 23:18:30 +0800

    https://github.com/JoyceChen1106/leetcode.git

--- 28.find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.py
@@ -7,10 +7,10 @@
 # @lc code=start
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        l1 = len(haystack)
+        l1 = len(haystack) 
         l2 = len(needle)
-        for i in range(l1-l2+1):
-            if haystack[i:i+l2] == needle:
+        for i in range(l1-l2+1): #兩兩字串相比並向後位移l1-l2+1步剛好到最後一個
+            if haystack[i:i+l2] == needle: #如果內容相等則找到
                 return i
         return -1
         
